cd7ab3025e949d6139a96d9e68acfc6cd8e6118b
vigneshA46 <156639506+vigneshA46@users.noreply.github.com>
2024-01-25 22:25:49 +0530

    Add files via upload

---
+++ todo list/js/app.js
@@ -0,0 +1,28 @@
+const inputBox = document.getElementById("input-box");
+const listcontainer = document.getElementById("listcontainer");
+
+function addTask() {
+  if (inputBox.value === "") {
+    alert("write something");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listcontainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+}
+
+listcontainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+    }
+  },
+  false
+);
